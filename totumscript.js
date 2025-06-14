16857a9a94b4bf200ccffe3415053d5bebee6d84
Xenoxia <116961211+enchamersy@users.noreply.github.com>
2025-06-14 23:35:08 +0100

    Add files via upload
    Removed Translucent UI to TotumOS page, Removed border around translucent elements, tidied up TotumOS page

--- totumscript.js
+++ totumscript.js
@@ -1,3 +1,11 @@
   window.addEventListener('load', function() {
     document.getElementById('loader').style.display = 'none';
   });
+window.addEventListener('scroll', function() {
+  const footer = document.querySelector('footer');
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    footer.style.display = 'block';
+  } else {
+    footer.style.display = 'none';
+  }
+});
